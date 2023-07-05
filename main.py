f77a8778ce80349681aa72e251e774415388165a
KinuTheDragon <91227747+KinuTheDragon@users.noreply.github.com>
2023-07-05 00:51:55 +0000

    Updated to allow for autopush to ShowierData's server

--- main.py
+++ main.py
@@ -3,9 +3,43 @@ from load import blocks, items, enchants, communist_items, artifacts
 from block import Block
 from item import Item
 from enchantment import Enchantment
-import re
+from flask.app import HTTPException
+import re, hashlib, hmac, os
+
+if not "REPL_ID" in os.environ:
+    from dotenv import load_dotenv
+    load_dotenv()
+
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
 
 app = Flask(__name__)
+
+@app.post("/api/webhook")
+def github_webhook():
+    data = request.get_data()
+    verify_signature(data,
+        os.environ["github_secret"],
+        request.headers.get("x-hub-signature-256"))
+
+    os.system("git reset --hard && git pull")
+
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
 
 @app.route("/")
 def index():
